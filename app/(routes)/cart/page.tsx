6846441015dfc5b70892901958290a61a9cd9bc8
Collins77 <collinsmuemah@gmail.com>
2024-01-01 09:00:27 +0300

    hydration-error

--- app/(routes)/cart/page.tsx
+++ app/(routes)/cart/page.tsx
@@ -7,16 +7,17 @@ import CartItem from "./components/cart-item";
 import Summary from "./components/summary";
 
 const CartPage = () => {
+    const [isMounted, setIsMounted] = useState(false);
     const cart = useCart();
-    // const [isMounted, setIsMounted] = useState(false);
 
-    // useEffect(() => {
-    //     setIsMounted(false);
-    // }, []);
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
 
-    // if(!isMounted) {
-    //     return null;
-    // }
+    if(!isMounted) {
+        return null;
+    }
+
 
     return ( 
         <div className="bg-white">
